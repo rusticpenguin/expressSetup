354e01b82b1b36d69bfa11982f3e15aab56c01c5
rusticpenguin <georgechios.music@gmail.com>
2018-07-31 11:08:28 -0600

    Added /1 to url to look at index 0 of json

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 9000;
 const cors = require("cors");
-const cohortsData = require("cohorts.js")
+const cohortsData = require("./cohorts.js")
 
-app.use(cors())
+app.use(cors());
 
 app.get("/",(req, res, next) => {
     res.json(cohortsData)
-})
+});
+
+
+app.get("/1",(req, res, next) => {
+    res.json(cohortsData[0])
+});
 
 app.listen(port,() => {
     console.log(`Free beans on port ${port}`)
-})
+});
